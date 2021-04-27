af2e2c8ea1e663722417cc8ddbeacd1b38d82653
Ludmylla Arielly <ludmay4@gmail.com>
2021-04-27 17:30:25 -0300

    adding try cath on userResource

--- src/main/java/com/ludmylla/user/api/resource/UserResource.java
+++ src/main/java/com/ludmylla/user/api/resource/UserResource.java
@@ -3,10 +3,6 @@ package com.ludmylla.user.api.resource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +13,6 @@ import com.ludmylla.user.api.model.JWTAuthenticationResponse;
 import com.ludmylla.user.api.model.User;
 import com.ludmylla.user.api.model.dto.UserCreateDTO;
 import com.ludmylla.user.api.model.dto.UserLoginDTO;
-import com.ludmylla.user.api.security.JWTTokenProvider;
 import com.ludmylla.user.api.services.UserService;
 
 @RestController
@@ -27,25 +22,26 @@ public class UserResource {
 	@Autowired
 	private UserService userService;
 
-	@Autowired
-	private AuthenticationManager authenticationManager;
-
-	@Autowired
-	private JWTTokenProvider jwtTokenProvider;
-
 	@PostMapping("/signin")
 	public ResponseEntity<JWTAuthenticationResponse> userAuthentication(@RequestBody UserLoginDTO userLoginDTO) {
-		User user = UserMapper.INSTANCE.toUser(userLoginDTO);
-		String token = userService.userAuthentication(user);
-		
-		return ResponseEntity.ok(new JWTAuthenticationResponse(token));
+		try {
+			User user = UserMapper.INSTANCE.toUser(userLoginDTO);
+			String token = userService.userAuthentication(user);
+			return ResponseEntity.ok(new JWTAuthenticationResponse(token));
+		}catch (Exception e) {
+			return new ResponseEntity<JWTAuthenticationResponse>(HttpStatus.UNAUTHORIZED);
+		}
 	}
 
 	@PostMapping("/signup")
 	public ResponseEntity<String> createUser(@RequestBody UserCreateDTO userCreateDTO) {
-		User user = UserMapper.INSTANCE.toUser(userCreateDTO);
-		userService.createUser(user);
-		return ResponseEntity.status(HttpStatus.CREATED).body("User registered successfully. ");
+		try {
+			User user = UserMapper.INSTANCE.toUser(userCreateDTO);
+			userService.createUser(user);
+			return ResponseEntity.status(HttpStatus.CREATED).body("User registered successfully. ");
+		}catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(" Failed to create user: " + e.getMessage());
+		}
 	}
 
 }
